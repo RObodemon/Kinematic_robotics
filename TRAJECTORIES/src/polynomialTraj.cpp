c8df90a2908bf17ac3555392b61b14e14864cd3d
Hengyue <45977195+RObodemon@users.noreply.github.com>
2022-12-28 00:28:12 -0500

    Add files via upload

--- TRAJECTORIES/src/polynomialTraj.cpp
+++ TRAJECTORIES/src/polynomialTraj.cpp
@@ -19,7 +19,7 @@ namespace eleTraj
         std::vector<double> traj(n+2,0);
         double temp = (t1-t0)/n;
         
-        std::ofstream fout("add your own directories/data_plot/linear_trj.txt",std::ios::out);
+        std::ofstream fout("C:/Users/Henyue Guan/Desktop/DSC/Robot/Robotics Arm/Project/Trajectories/data_plot/linear_trj.txt",std::ios::out);
         if (fout.is_open() == false)
         {
             std::cout << "open file" << " " << "fails\n";
@@ -33,7 +33,7 @@ namespace eleTraj
             }
             double t = t0+temp*i;
             traj[i] = a0+a1*t;
-            fout<<t<<" "<<traj[i]<<std::endl;
+            fout<<t<<" "<<traj[i]<<" "<<a1<<" "<<0<<std::endl;
         }
         traj[n] = a1;
         traj[n+1] = 0;
@@ -42,7 +42,7 @@ namespace eleTraj
     }
 
     // Parabolic trajectory (constant acceleration)
-    std::vector<double> parabolicTraj(double t0,double t1, 
+    std::vector<double> parabolicTraj(double t0,double t1, double tf,
         double q0,double q1, double v0, double v1, int n)
     {
         double a0 = q0;
@@ -50,41 +50,44 @@ namespace eleTraj
         double T = t1-t0;
         double h = 0;
         if(q1>=q0)
-        {
             h = q1-q0;
-        }
         else
-        {
             h = q0-q1;
-        }
-        double a2 = 2.0*(h-v0*T)/T*T;
 
+        double a2 = 2.0*(h-v0*T)/T*T;
         double a3 = (q0+q1)/2;
         double a4 = 2*h/T-v1;
         double a5 = 2*(v1*T-h)/(T*T);
 
-        std::vector<double> traj(2*n+1,0);
+        std::vector<double> traj(2*n+2,0);
         double temp = T/n;
-        std::string filename = R"(data_plot\parabolic_Trj.txt)";
-        std::ofstream fout;
+        std::ofstream fout("C:/Users/Henyue Guan/Desktop/DSC/Robot/Robotics Arm/Project/Trajectories/data_plot/Parabolic_trj.txt",std::ios::out);
+        if (fout.is_open() == false)
+        {
+            std::cout << "open file" << " " << "fails\n";
+        }
+
+        traj[2*n] = 4*(h-v0*T)/T/T; // acceleration
         for(int i=0;i<n/2;i++)
         {
             double t = temp*i;
-            traj[i] = a0+a1*t+a2*t*t;
-            traj[i+n] = a1+2*a2*t;
+            traj[i] = a0+a1*(t-t0)+2/T/T*(h-v0*T)*(t-t0)*(t-t0);  // position
+            traj[i+n] = v0+4/T/T*(h-v0*T)*(t-t0);     // velocity
+            fout<<t<<" "<<traj[i]<<" "<<traj[i+n]<<" "<<traj[2*n]<<std::endl; // the forth column is acceleration, which is a constant
         }
-        for(int i = n/2;i<n;i++)
-        {
-            double t = temp*t;
-            traj[i] =  a3+a4*t+a5*t*t;
-            traj[i+n] = a4+2*a5*t;
-        }
-        traj[2*n] = 4*(h-v0*T)/T/T;
-        fout.close();
 
+        traj[2*n+1] = 4*(v1*T-h)/T/T;  // acceleration
+        for(int i = n/2;i<=n;i++)
+        {
+            double t = temp*i;
+            traj[i] =  a3+(2*h/T-v1)*(t-tf)+2/T/T*(v1*T-h)*(t-tf)*(t-tf);  // postion
+            traj[i+n] = 2*h/T-v1+4/T/T*(v1*T-h)*(t-tf);      // velocity
+            fout<<t<<" "<<traj[i]<<" "<<traj[i+n]<<" "<<traj[2*n+1]<<std::endl;  // the forth column is acceleration, which is a constant
+        }
+
+        fout.close();
         return traj;
     }
-
 
 }
 #endif
