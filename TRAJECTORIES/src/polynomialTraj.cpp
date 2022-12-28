04a0db09201e59cf44073f9153f87f18bf8c0c1e
Hengyue <45977195+RObodemon@users.noreply.github.com>
2022-12-27 23:04:11 -0800

    Update polynomialTraj.cpp

--- TRAJECTORIES/src/polynomialTraj.cpp
+++ TRAJECTORIES/src/polynomialTraj.cpp
@@ -45,19 +45,22 @@ namespace eleTraj
     std::vector<double> parabolicTraj(double t0,double t1, double tf,
         double q0,double q1, double v0, double v1, int n)
     {
+        double T  = t1-t0;
+        double Ta = tf-t0; 
+        double Td = t1-tf;
+
         double a0 = q0;
         double a1 = v0;
-        double T = t1-t0;
-        double h = 0;
+        double h  = 0;
         if(q1>=q0)
             h = q1-q0;
         else
             h = q0-q1;
 
-        double a2 = 2.0*(h-v0*T)/T*T;
-        double a3 = (q0+q1)/2;
-        double a4 = 2*h/T-v1;
-        double a5 = 2*(v1*T-h)/(T*T);
+        double a2 = (2.0*h-v0*(T+Ta)-v1*Td)/(2*T*Ta);
+        double a3 = (2.0*q1*Ta+Td*(2*q0+Ta*(v0-v1)))/(2*T);
+        double a4 = (2.0*h-v0*Ta-v1*Td)/T;
+        double a5 = -(2.0*h-v0*Ta-v1*(T+Td))/(2*T*Td);
 
         std::vector<double> traj(2*n+2,0);
         double temp = T/n;
@@ -67,21 +70,22 @@ namespace eleTraj
             std::cout << "open file" << " " << "fails\n";
         }
 
-        traj[2*n] = 4*(h-v0*T)/T/T; // acceleration
-        for(int i=0;i<n/2;i++)
+        traj[2*n] = 2*a2; // acceleration
+        int sec = n*Ta/T;
+        for(int i=0;i<sec;i++)
         {
-            double t = temp*i;
-            traj[i] = a0+a1*(t-t0)+2/T/T*(h-v0*T)*(t-t0)*(t-t0);  // position
-            traj[i+n] = v0+4/T/T*(h-v0*T)*(t-t0);     // velocity
+            double t = temp*i+t0;
+            traj[i] = a0+a1*(t-t0)+a2*(t-t0)*(t-t0);  // position
+            traj[i+n] = a1+2*a2*(t-t0);     // velocity
             fout<<t<<" "<<traj[i]<<" "<<traj[i+n]<<" "<<traj[2*n]<<std::endl; // the forth column is acceleration, which is a constant
         }
 
-        traj[2*n+1] = 4*(v1*T-h)/T/T;  // acceleration
-        for(int i = n/2;i<=n;i++)
+        traj[2*n+1] = 2*a5;  // acceleration
+        for(int i = sec;i<=n;i++)
         {
-            double t = temp*i;
-            traj[i] =  a3+(2*h/T-v1)*(t-tf)+2/T/T*(v1*T-h)*(t-tf)*(t-tf);  // postion
-            traj[i+n] = 2*h/T-v1+4/T/T*(v1*T-h)*(t-tf);      // velocity
+            double t = temp*i+t0;
+            traj[i] =  a3+a4*(t-tf)+a5*(t-tf)*(t-tf);  // postion
+            traj[i+n] = a4+2*a5*(t-tf);      // velocity
             fout<<t<<" "<<traj[i]<<" "<<traj[i+n]<<" "<<traj[2*n+1]<<std::endl;  // the forth column is acceleration, which is a constant
         }
 
